5acf2ce32968dbc2835c7c2eac86773604076fcd
AJ ONeal <coolaj86@gmail.com>
2018-10-11 02:28:23 +0000

    remove dangling assignment

--- routers/user/auth.go
+++ routers/user/auth.go
@@ -1176,7 +1176,7 @@ func commonResetPassword(ctx *context.Context) *models.User {
 func ResetPasswd(ctx *context.Context) {
 	ctx.Data["IsResetForm"] = true
 
-	_ = commonResetPassword(ctx)
+	commonResetPassword(ctx)
 
 	ctx.HTML(200, tplResetPassword)
 }
